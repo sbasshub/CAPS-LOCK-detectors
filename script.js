c0a7bcd6e78159c013ac9004c6fd4d8d7932b37d
Sebastian DS <sbasspro23@gmail.com>
2020-12-22 20:48:09 +0700

    adding the script logic

---
+++ script.js
@@ -0,0 +1,10 @@
+const capsMessage = document.querySelector('#caps-message');
+
+window.addEventListener('keyup', event => {
+    if (event.getModifierState ('CapsLock')) {
+        capsMessage.style.display = 'flex';
+    }
+    else {
+        capsMessage.style.display = 'none';
+    }
+});
